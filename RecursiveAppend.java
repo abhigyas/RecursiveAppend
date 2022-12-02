215bae030e9670a42b85f1b82491e91b3e915044
Abhigya Sinha <117308115+abhigyas@users.noreply.github.com>
2022-12-01 20:59:33 -0500

    Add files via upload

---
+++ RecursiveAppend.java
@@ -0,0 +1,23 @@
+/*************************************************************************
+ * Compilation: javac RecursiveAppend.java
+ * Execution: java RecursiveAppend
+ *
+ * @author: Abhigya Sinha
+ *
+ *************************************************************************/
+
+public class RecursiveAppend {
+
+    // Returns the original string appended to the original string n times
+    public static String appendNTimes(String original, int n) {
+        if (n < 1) {
+            return original;
+        } else {
+            return original + appendNTimes(original, n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(appendNTimes("Siddharth", 1));
+    }
+}
